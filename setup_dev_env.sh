d0dc51eb7ce6bbdbb4d5890dcbaf2540c9523faa
Victor Vieux <vieux@docker.com>
2014-06-08 21:44:06 +0000

    update golang images

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 docker run -d -p 127.0.0.1:6379:6379 --name redis-master crosbymichael/redis
-cd worker && docker build -t worker .
-cd server && go build && ./server
+cd workers
+docker build -t worker:1.2.2 1.2.2/
+docker build -t worker:1.3rc1 1.3rc1/
+docker tag worker:1.2.2 worker:latest
+cd ../server
+go build && ./server
