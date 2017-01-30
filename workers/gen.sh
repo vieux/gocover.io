684a31b384760b02228746c8efc0d5cd4740c6a7
Victor Vieux <victorvieux@gmail.com>
2017-01-30 15:39:15 -0800

    bump go

--- workers/gen.sh
+++ workers/gen.sh
@@ -1,5 +1,5 @@
-VERSIONS="1.2.2 1.3 1.3.1 1.3.2 1.3.3 1.4 1.4.1 1.4.2 1.4.3 1.5 1.5.1 1.5.2 1.5.3 1.6 1.6.1 1.6.2 1.6.3 1.7 1.7.1"
-LATEST="1.7.1"
+VERSIONS="1.6.4 1.7.5"
+LATEST="1.7.5"
 
 for version in $VERSIONS; do
     docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version . 
--- workers/push.sh
+++ workers/push.sh
@@ -1,4 +1,4 @@
-VERSIONS="1.2.2 1.3 1.3.1 1.3.2 1.3.3 1.4 1.4.1 1.4.2 1.4.3 1.5 1.5.1 1.5.2 1.5.3 1.6 1.6.1 1.6.2 1.6.3 1.7 latest"
+VERSIONS="1.6.4 1.7.5 latest"
 for version in $VERSIONS; do
     docker push vieux/gocover:$version
 done
