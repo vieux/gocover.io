431c6cc351ee11286a1088594dd44b10ff6698a3
Victor Vieux <victorvieux@gmail.com>
2017-12-11 15:30:05 -0800

    add 1.9.2 and 1.10beta1
    Signed-off-by: Victor Vieux <victorvieux@gmail.com>

--- workers/gen.sh
+++ workers/gen.sh
@@ -1,8 +1,8 @@
-VERSIONS="1.8"
-LATEST="1.8"
+VERSIONS="1.8 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.9 1.9.1 1.9.2"
+LATEST="1.9.2"
 
 for version in $VERSIONS; do
-    docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version . 
+    docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version .
 done
     
 docker tag vieux/gocover:$LATEST vieux/gocover:latest
