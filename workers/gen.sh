88cd66de4fe8214909c9f82c071321168e76188f
Victor Vieux <victorvieux@gmail.com>
2018-01-22 14:25:45 -0800

    add 1.9.3

--- workers/gen.sh
+++ workers/gen.sh
@@ -1,5 +1,5 @@
-VERSIONS="1.8 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.9 1.9.1 1.9.2"
-LATEST="1.9.2"
+VERSIONS="1.8 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.9 1.9.1 1.9.2 1.9.3"
+LATEST="1.9.3"
 
 for version in $VERSIONS; do
     docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version .
--- workers/push.sh
+++ workers/push.sh
@@ -1,4 +1,4 @@
-VERSIONS="1.8 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.9 1.9.1 1.9.2 latest"
+VERSIONS="1.8 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.9 1.9.1 1.9.2 1.9.3 latest"
 
 for version in $VERSIONS; do
     docker push vieux/gocover:$version
