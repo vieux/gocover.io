cd22db7963672c8d2ca8bf39747a19e6d51658dd
Victor Vieux <victorvieux@gmail.com>
2018-03-29 14:17:51 -0700

    bump go
    Signed-off-by: Victor Vieux <victorvieux@gmail.com>

--- workers/gen.sh
+++ workers/gen.sh
@@ -1,11 +1,11 @@
-VERSIONS="1.8 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.9 1.9.1 1.9.2 1.9.3 1.9.4 1.10"
-LATEST="1.10"
+VERSIONS="1.8 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.9 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.10 1.10.1"
+LATEST="1.10.x"
 
 for version in $VERSIONS; do
-    docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version .
+    echo "docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version ."
 done
 
-docker tag vieux/gocover:1.8.5 vieux/gocover:1.8.x
-docker tag vieux/gocover:1.9.4 vieux/gocover:1.9.x
-docker tag vieux/gocover:1.10 vieux/gocover:1.10.x
-docker tag vieux/gocover:$LATEST vieux/gocover:latest
+echo "docker tag vieux/gocover:1.8.5 vieux/gocover:1.8.x"
+echo "docker tag vieux/gocover:1.9.5 vieux/gocover:1.9.x"
+echo "docker tag vieux/gocover:1.10.1 vieux/gocover:1.10.x"
+echo "docker tag vieux/gocover:$LATEST vieux/gocover:latest"
