0c7c5277c81cbaac1d0b4bf0365707cb63d86135
Victor Vieux <victorvieux@gmail.com>
2020-03-31 07:35:48 +0000

    go1.14

--- workers/gen.sh
+++ workers/gen.sh
@@ -1,15 +1,15 @@
-VERSIONS="1.12 .1.12.1 1.12.2 1.12.3 1.12.4 1.12.5 1.12.6 1.12.7 1.12.8 1.12.9 1.13"
-LATEST="1.13.x"
+VERSIONS="1.13.9 1.14 1.14.1"
+LATEST="1.14.x"
 
 for version in $VERSIONS; do
     docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version .
     docker push vieux/gocover:$version
 done
 
-docker tag vieux/gocover:1.12.9 vieux/gocover:1.12.x
-docker push vieux/gocover:1.12.x
-docker tag vieux/gocover:1.13 vieux/gocover:1.13.x
+docker tag vieux/gocover:1.13.9 vieux/gocover:1.13.x
 docker push vieux/gocover:1.13.x
+docker tag vieux/gocover:1.14.1 vieux/gocover:1.14.x
+docker push vieux/gocover:1.14.x
 docker tag vieux/gocover:$LATEST vieux/gocover:latest
 docker push vieux/gocover:latest
 
