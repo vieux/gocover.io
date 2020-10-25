93c3892debea86e52d6182ef7cc9bc8e41959c41
Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
2020-10-25 21:25:32 +0100

    Get total coverage result using the go tool cover command

--- workers/gocover.sh
+++ workers/gocover.sh
@@ -9,7 +9,7 @@ fi
 
 cd $1
 
-number=`go test -covermode=count -coverprofile=coverage.out ./... | grep coverage | cut -d ' ' -f 2 | sed 's/%//g'`
+go test -covermode=count -coverprofile=coverage.out ./...
 
 if [ $? -gt 0 ]; then
     echo "Cannot test '$1'" >&2
@@ -27,5 +27,8 @@ if [ $? -gt 0 ]; then
     echo "Cannot get coverage of '$1'" >&2
     exit 4
 fi
+
+# Get total coverage result from each function:
+number=$(go tool cover -func coverage.out | sed -E -n 's/^total:.*\s+([0-9\.]+)%$/\1/p')
 
 echo "<!-- cov:$number -->"
