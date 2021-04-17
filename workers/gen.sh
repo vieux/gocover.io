834fea7c4a632b9be4381b2d859b6d3468998980
Victor Vieux <victorvieux@gmail.com>
2021-04-17 02:03:22 -0700

    update 1.16.x

--- workers/gen.sh
+++ workers/gen.sh
@@ -1,15 +1,15 @@
-VERSIONS="1.13.9 1.14 1.14.1"
-LATEST="1.14.x"
+VERSIONS="1.16.3 1.16.2 1.16.1 1.16 1.15.11"
+LATEST="1.16.x"
 
 for version in $VERSIONS; do
     docker build --build-arg GO_VERSION=$version -t vieux/gocover:$version .
     docker push vieux/gocover:$version
 done
 
-docker tag vieux/gocover:1.13.9 vieux/gocover:1.13.x
-docker push vieux/gocover:1.13.x
-docker tag vieux/gocover:1.14.1 vieux/gocover:1.14.x
-docker push vieux/gocover:1.14.x
+docker tag vieux/gocover:1.15.11 vieux/gocover:1.15.x
+docker push vieux/gocover:1.15.x
+docker tag vieux/gocover:1.16.3 vieux/gocover:1.16.x
+docker push vieux/gocover:1.16.x
 docker tag vieux/gocover:$LATEST vieux/gocover:latest
 docker push vieux/gocover:latest
 
